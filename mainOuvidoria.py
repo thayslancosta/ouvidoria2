8a0e56bd1e300ec0eff5e01cccd422039c74d8d3
Thayslan Costa <thayslan.costa@gmail.com>
2025-04-06 04:09:06 -0300

    Main atualizado com novas funções

--- mainOuvidoria.py
+++ mainOuvidoria.py
@@ -1,3 +1,4 @@
+#Função para obter inteiro do user para o Menu principal
 def opcaoUser():
     while True:
         try:
@@ -6,11 +7,32 @@ def opcaoUser():
         except ValueError:
             print("Erro! Digite um número válido.")
 
+#Dicionário com os tipos de manifestação (colunas do BD)
+tiposManifestacao = {
+    1: "Reclamação",
+    2: "Elogio",
+    3: "Melhoria"
+    }
 
-def subOpcaoUser():
+#Função para obter do user o tipo de manifestação que ele está buscando (Reclamação, Elogio, Melhoria) ou redirecioná-lo ao Menu Principal
+def escolherTipoManifestacao():
     while True:
         try:
-            subopcao = int(input("Digite sua escolha: "))
-            return subopcao  
+            tipoInput = int(input(
+                                    "Digite 1 para Reclamações \n"
+                                    "Digite 2 para Elogios \n"
+                                    "Digite 3 para Melhorias \n"
+                                    "Digite 4 para retornar ao menu \n"
+                                    "Informe o tipo de manifestação que você está buscando: "
+                                    ))
+            if tipoInput == 4:
+                return None
+            
+            userTipoInformado = tiposManifestacao.get(tipoInput)
+
+            if userTipoInformado:
+                return userTipoInformado
+            else:
+                print("Opção inválida. Tente novamente!\n")
         except ValueError:
-            print("Erro! Digite um número válido.")
+            print("Entrada inválida. Digite um número de 1 a 4.\n")
